2bf0e78cb57233fbb071960bbf5a2032ddda95d5
LETICIA FERNANDA PEREIRA <leticia.6978@aluno.pr.senac.br>
2024-05-02 08:41:06 -0300

    Dart

---
+++ aula01.dart
@@ -0,0 +1,11 @@
+void main() {
+  //variavel de tipo dinâmico
+  var nome = 'Dieimes';
+
+  //variaveis com tipos especificos
+  int idade = 33;
+  double altura = 1.75;
+  String cidade = 'Ivaiporã';
+  bool possuiCarro = true;
+ 
+}
