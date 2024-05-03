4b1bdacda8b3f9b4a6d4c36889dbe41dd7545d77
LETICIA FERNANDA PEREIRA <leticia.6978@aluno.pr.senac.br>
2024-05-03 08:51:46 -0300

    tabuada

---
+++ .vscode/tabuadafor.dart
@@ -0,0 +1,10 @@
+void main() {
+  int numero = 5; 
+
+  print("Tabuada do $numero:");
+
+  for (int i = 1; i <= 10; i++) {
+    int resultado = numero * i;
+    print("$numero x $i = $resultado");
+  }
+}
